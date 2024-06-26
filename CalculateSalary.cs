e946bc1a23fe0345dd4c0fa204ac4072335a1772
chaniadler <c.adler6738@gmail.com>
2024-06-26 15:04:24 +0300

    Add files via upload

---
+++ CalculateSalary.cs
@@ -0,0 +1,6 @@
+﻿namespace SalaryApi
+{
+    public class CalculateSalary
+    {
+    }
+}
---
+++ SalaryCalculationResult.cs
@@ -0,0 +1,17 @@
+﻿namespace SalaryApi
+{
+    public class SalaryCalculationResult
+    {
+        public decimal BaseSalary { get; set; }
+        public decimal SeniorityBonus { get; set; }
+        public decimal TotalSeniorityBonusPercentage { get; set; }
+        public decimal TotalSeniorityBonusAmount { get; set; }
+        public decimal LawBonus { get; set; }
+        public decimal TotalLawBonus { get; set; }
+        public decimal SalaryBeforeIncrease { get; set; }
+        public decimal SalaryIncreasePercentage { get; set; }
+        public decimal TotalSalaryIncrease { get; set; }
+        public decimal FinalSalary { get; set; }
+    }
+
+}
